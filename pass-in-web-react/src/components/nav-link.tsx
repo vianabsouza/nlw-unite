ad21f9c1d3775fda8c4def255b12d390cf4d00b5
Barbara Viana <vianabsouza@gmail.com>
2024-04-03 14:29:58 -0300

    create nav link component

---
+++ pass-in-web-react/src/components/nav-link.tsx
@@ -0,0 +1,11 @@
+import { ComponentProps } from "react"
+
+interface NavLinksProps extends ComponentProps<'a'> {
+  children: string
+}
+
+export function NavLink(props: NavLinksProps) {
+  return (
+    <a {...props} className="font-medium text-sm">{props.children}</a>
+  )
+}
