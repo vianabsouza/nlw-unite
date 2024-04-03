7ef215f5fd55b6a03c78f20cb923ca632a9c4f7c
Barbara Viana <vianabsouza@gmail.com>
2024-04-02 23:32:55 -0300

    add tailwind to the project and create header

--- pass-in-web-react/src/app.tsx
+++ pass-in-web-react/src/app.tsx
@@ -1,5 +1,9 @@
+import { Header } from "./components/header";
+
 export function App() {
   return (
-    <h1></h1>
+    <div className="max-w-[1216px] mx-auto py-5 flex flex-col gap-5">
+      <Header />
+    </div>
   )
 }
